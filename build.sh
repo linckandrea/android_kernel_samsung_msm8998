b745be270d225653d0ac01a2dd885ace00809c9e
Wei Wang <wvw@google.com>
2017-09-10 14:27:05 -0700

    do not call trace_printk on non-debug build
    trace_printk will cause trace_printk_init_buffers executed in kernel
    start. Remove them from non-debug build.
    
    Test: see nasty message gone in dmesg
    Bug: 64215528
    Change-Id: I82b8435a3cf36123608aae572c843db8ad86ac8a
    Signed-off-by: Wei Wang <wvw@google.com>

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ echo
 branch=$(git symbolic-ref --short HEAD)
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 last_commit=$(git rev-parse --verify --short=8 HEAD)
+export LOCALVERSION="-Armonia-Kernel-${branch_name}/${last_commit}"
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
@@ -26,4 +27,13 @@ echo "let's build"
 echo 
 make O=out -j$(nproc --all) CONFIG_DEBUG_SECTION_MISMATCH=y
 
+
+echo
+echo "copying new files"
+echo
+cp ./out/arch/arm64/boot/Image.gz-dtb ./AnyKernel3/
+cd ./AnyKernel3/
+zip -r9 Armonia-kernel-SM-T830"$version"-"$branch"-"$last_commit".zip * -x .git README.md *placeholder
+
+cd ..
 echo THE END
