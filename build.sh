2303e4943fac64408b4466acf86afb931dbcf12a
linckandrea <linckandrea01@gmail.com>
2025-03-02 21:49:55 +0100

    make it build

---
+++ build.sh
@@ -0,0 +1,30 @@
+echo
+echo "Setup"
+echo
+git submodule update --init --recursive
+branch=$(git symbolic-ref --short HEAD)
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+last_commit=$(git rev-parse --verify --short=8 HEAD)
+mkdir -p out
+export ARCH=arm64
+export SUBARCH=arm64
+make O=out clean
+make O=out mrproper
+
+echo
+echo "Issue Build Commands"
+echo
+export CROSS_COMPILE=./scripts/toolchain/gcc-cfp/gcc-cfp-single/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+echo
+echo "Set DEFCONFIG"
+echo 
+make O=out ARCH=arm64 gts4lwifi_eur_open_defconfig
+
+
+echo
+echo "let's build"
+echo 
+make O=out -j$(nproc --all) CONFIG_DEBUG_SECTION_MISMATCH=y
+
+echo THE END
